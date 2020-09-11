7a37d8438835878da23cb3d4f248aae2390b57c5
Autumn Teo <autumnteo96@gmail.com>
2020-09-11 12:02:22 +0800

    Updates

---
+++ precache-manifest.77dac664e392b884a342ed789eb63055.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0f827484558f174042458fc176fd278",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e595d3384530d8026c71",
+    "url": "/static/css/main.d2f2a00d.chunk.css"
+  },
+  {
+    "revision": "18f109306ea3f8e3b07a",
+    "url": "/static/js/2.6bb0d36b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6bb0d36b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e595d3384530d8026c71",
+    "url": "/static/js/main.47a34e89.chunk.js"
+  },
+  {
+    "revision": "f00c9f41b18864465862",
+    "url": "/static/js/runtime-main.094ad2ef.js"
+  }
+]);
